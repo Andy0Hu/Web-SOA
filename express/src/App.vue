577cc59f78228f4e4ef831f9caaeeff588ab1bc0
PtNan <imasxu@gmail.com>
2019-11-10 13:53:20 +0800

    init frontend

---
+++ express/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>
+      <router-link to="/">Login</router-link>|
+      <router-link to="/register">Register</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="stylus"></style>
