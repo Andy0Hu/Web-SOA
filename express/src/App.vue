0dd0b87b453ddb7a1eb5638970951c16b7448744
PtNan <imasxu@gmail.com>
2019-11-10 23:42:06 +0800

    login layout

--- express/src/App.vue
+++ express/src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    <div>
-      <router-link to="/">Login</router-link>|
-      <router-link to="/register">Register</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
-<style lang="stylus"></style>
+<style lang="less"></style>
